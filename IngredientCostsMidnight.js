d5b81d866feb01d999812dacf2c60118a940c196
Harize Tamara <61130507+HarizeTamara@users.noreply.github.com>
2025-07-01 20:50:29 -0400

    Update IngredientCostsMidnight.js

--- IngredientCostsMidnight.js
+++ IngredientCostsMidnight.js
@@ -1,7 +1,7 @@
 export const ingredientCosts = {
-    "Lingote de Ferro": 180.0,
-    "Lingote de Cobre": 210.0,
-    "Lingote de Aluminio": 175.0,
+    "Lingote de Ferro": 240.0,
+    "Lingote de Cobre": 285.0,
+    "Lingote de Aluminio": 210.0,
     "Plástico": 10.0,
     "Borracha": 10.0,
     "Maçarico": 15.0,
